04cf936f48f13b12f7fa2d8ccb09ac45eeae8852
beth-ouyang <65527698+beth-ouyang@users.noreply.github.com>
2022-05-01 12:01:16 +0800

    add-setTimeout
    新增CD功能

--- server.js
+++ server.js
@@ -1,63 +1,82 @@
-const {readFileSync, promises: fsPromises} = require('fs');
+const { readFileSync, promises: fsPromises } = require('fs');
 const tmi = require('tmi.js');
-var use_cnt = 0;
-var foodsList = readFoodList('./food_list.txt');
-var stamp_arr = ['nlnlYes', 'nlnlOMG', 'BloodTrail', 'StinkyCheese', 'BabyRage', 'BrainSlug', 'KappaPride', 'PogChamp', 'TPFufun', 'cmonBruh', 'SwiftRage', 'PunOko', 'SabaPing', 'FamilyMan', 'BegWan', 'SeemsGood', 'OSFrog'];
 
+var door_open = true;
+var use_cnt = 0;
+var goodnoght_cnt = 0;
+var foodsList = readFoodList('./food_list.txt');
+var stamp_arr = ['nlnlYes', 'nlnlOMG', 'BloodTrail', 'StinkyCheese', 'BabyRage', 'BrainSlug', 'KappaPride', 'PogChamp', 'TPFufun', 'cmonBruh', 'SwiftRage', 'PunOko', 'SabaPing', 'FamilyMan', 'BegWan', 'SeemsGood', 'OSFrog', 'RaccAttack'];
 
 const client = new tmi.Client({
-	connection:{
-		reconnect: true
-	},
-	channels: ['never_loses'],
-	identity: {
-		username: 'username',
-		password: 'password' //twitchgenerator.com
-	},
+    connection: {
+        reconnect: true
+    },
+    channels: ['never_loses'],
+    identity: {
+        username: 'your_username',
+        password: 'your_password' //get from → twitchgenerator.com
+    },
 });
 
-// console.log(selectFood(foodsList))
 
 client.connect();
 client.on('connected', onConnectedHandler);
 
 client.on('message', echoDinner);
 
+
 //////////////////////////////////////////////////////////
 
 function echoDinner(channel, tags, message, self, food_list = foodsList) {
     if (self) { return; }
-
-    const commandName = replaceEMOTES(message).trim();
     var emote = selectEMOTES();
 
-
-    if (commandName === '!晚餐時間') {
-        use_cnt += 1
+    if (message.includes('!晚餐時間') & door_open == true) {
         var food = selectFood(food_list);
+
+        door_open = false;
+        use_cnt += 1;
+
         client.say(channel, `@${tags["display-name"]} 晚餐吃${food} ${emote} `);
+        console.log(`* 機器皮皮蝦被呼喚了 ${use_cnt}次`);
 
-        console.log(`* ${commandName} 被使用了 ${use_cnt}次`);
+        setTimeout(function() { door_open = true }, 10000);
     }
 
-    if (commandName === '!午餐時間') {
-        use_cnt += 1
+    if (message.includes('!午餐時間') & door_open == true) {
         var food = selectFood(food_list);
-        client.say(channel, `@${tags["display-name"]} 午餐吃${food} ${emote} `);
 
-        console.log(`* ${commandName} 被使用了 ${use_cnt}次`);
+        door_open = false
+        use_cnt += 1
+
+        client.say(channel, `@${tags["display-name"]} 午餐吃${food} ${emote} `);
+        console.log(`* 機器皮皮蝦被呼喚了 ${use_cnt}次`);
+
+        setTimeout(function() { door_open = true }, 10000);
     }
 
-    sleep(5000);
+    if (message.includes('!睡覺時間') & door_open == true) {
+        var food = selectFood(food_list);
+
+        door_open = false
+        goodnoght_cnt += 1
+
+        client.say(channel, `@${tags["display-name"]} 古德奈 <3 `);
+        console.log(`* 有 ${goodnoght_cnt} 個人加入了睡睡幫 zZZ`);
+
+        setTimeout(function() { door_open = true }, 10000);
+    }
+
+
 }
 
 
-function replaceEMOTES (str){
+function replaceEMOTES(str) {
     const rule = /[a-zA-Z\d]/ig;
-    return str.replaceAll(rule,"");
+    return str.replaceAll(rule, "");
 }
 
-function selectFood (list) {
+function selectFood(list) {
     const len = list.length;
     return list[Math.floor(Math.random() * len)];
 }
@@ -73,10 +92,6 @@ function readFoodList(filename) {
     return arr;
 }
 
-function onConnectedHandler (addr, port) {
+function onConnectedHandler(addr, port) {
     console.log(`* Connected to ${addr}:${port}`);
-}
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
 }
