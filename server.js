e530cca869694f470723be2c80f6e86b405a1e3c
beth-ouyang <65527698+beth-ouyang@users.noreply.github.com>
2023-02-16 11:34:33 +0800

    Change channels

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const client = new tmi.Client({
     connection: {
         reconnect: true
     },
-    channels: ['never_loses'],
+    channels: ['never_loses', 'achiaaaaka'],
     identity: {
         username: process.env.TWITCH_BOT_USERNAME,
         password: process.env.TWITCH_BOT_OATH_TOKEN
